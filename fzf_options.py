b25f392a50de01644c0cdb392c8445fd3e7911a0
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2023-09-15 19:52:08 +0900

    Added keep-right

--- fzf_options.py
+++ fzf_options.py
@@ -136,6 +136,7 @@ def get_default_mode_options(server_port):
         "--reverse",
         "--prompt",
         "DEFAULT> ",
+        "--keep-right",
         "--preview",
         get_preview(server_port),
         "--preview-window",
